53abd8324024cc07dcd25abc68b223370b5744c2
Lourdle Duan <lourdle@outlook.com>
2026-04-21 14:46:59 +0800

    common: keep only core utf8 helpers

--- common/common.cpp
+++ common/common.cpp
@@ -4,43 +4,10 @@
     #define NOMINMAX
     #include <Windows.h>
     #include <cwchar>
-#else
-    #include <strings.h>
+    #include <cstring>
 #endif
 
-int tchar_casecmp(const tchar* lhs, const tchar* rhs)
-{
 #ifdef _WIN32
-    return _wcsicmp(lhs, rhs);
-#else
-    return strcasecmp(lhs, rhs);
-#endif
-}
-
-std::string tchar_to_utf8(const tchar* value)
-{
-    if (!value) return {};
-
-#ifdef _WIN32
-    const int wide_len = static_cast<int>(wcslen(value));
-    const int utf8_len = WideCharToMultiByte(CP_UTF8, 0, value, wide_len, nullptr, 0, nullptr, nullptr);
-    if (utf8_len <= 0) return {};
-
-    std::string result(static_cast<size_t>(utf8_len), '\0');
-    if (WideCharToMultiByte(CP_UTF8, 0, value, wide_len, &result[0], utf8_len, nullptr, nullptr) <= 0)
-        return {};
-    return result;
-#else
-    return value;
-#endif
-}
-
-#ifdef _WIN32
-void setup_console_utf8()
-{
-    SetConsoleOutputCP(CP_UTF8);
-}
-
 std::wstring utf8_to_wstr(const char* utf8)
 {
     int len = static_cast<int>(strlen(utf8));
@@ -55,15 +22,6 @@ std::wstring utf8_to_wstr(const char* utf8)
     return wide;
 }
 #endif
-
-std::ifstream open_ifstream_utf8(const char* path, std::ios::openmode mode)
-{
-#ifdef _WIN32
-    return std::ifstream(utf8_to_wstr(path).c_str(), mode);
-#else
-    return std::ifstream(path, mode);
-#endif
-}
 
 std::ofstream open_ofstream_utf8(const char* path, std::ios::openmode mode)
 {
