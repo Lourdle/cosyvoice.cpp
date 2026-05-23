cdd87f8bbaf2e52e85768b000949d881aa119f93
Lourdle Duan <lourdle@outlook.com>
2026-05-23 14:43:50 +0800

    common: update default LLM max sequence length to 4096

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@ std::wstring utf8_to_wstr(const char* utf8);
 std::ifstream open_ifstream_utf8(const char* path, std::ios::openmode mode = std::ios::binary);
 std::ofstream open_ofstream_utf8(const char* path, std::ios::openmode mode = std::ios::binary | std::ios::trunc);
 
-#define COSYVOICE_DEFAULT_LLM_MAX_SEQ_LEN 8192
+#define COSYVOICE_DEFAULT_LLM_MAX_SEQ_LEN 4096
 #define STRINGIFY(x) #x
 #define EXPAND_AND_STRINGIFY(x) STRINGIFY(x)
 #define COSYVOICE_DEFAULT_LLM_MAX_SEQ_LEN_STR EXPAND_AND_STRINGIFY(COSYVOICE_DEFAULT_LLM_MAX_SEQ_LEN)
